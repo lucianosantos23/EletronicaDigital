aed4f14d966d0639f1bffa261bfede6f5e451040
lucianosantos23 <lucianoaraujodossantosfilho@gmail.com>
2025-10-22 13:48:50 -0300

    add: Contador Sincrono 4 bits

---
+++ ContadorSíncrono/contador_sincrono.v
@@ -0,0 +1,34 @@
+module contador_sequencial (
+    input wire clk,
+    input wire rst,
+    output reg [3:0] count
+);
+
+always @(posedge clk or posedge rst) begin
+    if (rst) begin
+        count <= 4'd1; // Inicia com 1
+    end else begin
+        case (count)
+            4'd1:  count <= 4'd3;
+            4'd3:  count <= 4'd5;
+            4'd5:  count <= 4'd0;
+            4'd0:  count <= 4'd2;
+            4'd2:  count <= 4'd4;
+            4'd4:  count <= 4'd6;
+            4'd6:  count <= 4'd7;
+            4'd7:  count <= 4'd8;
+            4'd8:  count <= 4'd9;
+            4'd9:  count <= 4'd10;
+            4'd10: count <= 4'd15;
+            4'd15: count <= 4'd14;
+            4'd14: count <= 4'd13;
+            4'd13: count <= 4'd12;
+            4'd12: count <= 4'd11;
+            4'd11: count <= 4'd1; // Reinicia sequência
+            default: count <= 4'd1;
+        endcase
+    end
+end
+
+endmodule
+
---
+++ ContadorSíncrono/contador_sincrono_tb.v
@@ -0,0 +1,34 @@
+`timescale 1ns/1ps
+
+module tb_contador_sequencial();
+
+    reg clk;
+    reg rst;
+    wire [3:0] count;
+
+    contador_sequencial dut (
+        .clk(clk),
+        .rst(rst),
+        .count(count)
+    );
+
+    initial clk = 0;
+    always #5 clk = ~clk;
+
+    initial begin
+        $display("Tempo\tclk\trst\tcount");
+        $monitor("%4dns\t%b\t%b\t%d", $time, clk, rst, count);
+    end
+
+    initial begin
+        rst = 1; 
+        #12;
+        rst = 0; 
+
+        #300;
+
+        $finish; 
+    end
+
+endmodule
+
